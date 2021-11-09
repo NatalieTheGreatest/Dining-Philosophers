74d4eef50e56dd46f44d67160e4fc1d767e7bccb
nat <natalie.friede@gmail.com>
2021-11-09 11:07:07 -0600

    Initial commit

---
+++ sleepingBarber.java
@@ -0,0 +1,102 @@
+//Sleeping barber
+//Developed mostly by Natalie Friede 010892127
+import java.io.Console;
+import java.util.Scanner;
+import java.util.concurrent.Semaphore;
+
+public class sleepingBarber{
+    // main is here
+    public static void main (String[] args){
+    System.out.println("IT DOES SOMETHING");
+    // Input sleepTimeBarber and numChairs from command line
+    int sleep = 5;
+    int numChairs = 3;
+    for (String s: args) {
+        System.out.println(s);
+    }
+
+    System.out.println(sleep + " " + numChairs + "chairs is sleep");
+    // Default sleepTimeBarber = 5, default numChairs = 3
+    // Print parameters.
+    // instantiate shop here.
+    barberShop shop = new barberShop(numChairs);
+    Barber barber = new Barber(shop);
+    CustomerGenerator custGen = new CustomerGenerator(shop);
+    Thread oneBarber = new Thread(barber);
+    Thread multipleCustGen = new Thread(custGen);
+    oneBarber.start();
+    multipleCustGen.start();
+    }
+  }
+    // Barber object that will become thread.
+class Barber implements Runnable{
+    barberShop shop;
+    // Need access to shop object.
+    public Barber(barberShop shop){
+    this.shop = shop;
+    }
+    public void run(){
+    //Simulate sleep by putting thread to sleep
+    while(true){
+    shop.cutHair();
+    }
+    }
+}
+    // Customer object that will become thread.
+class Customer implements Runnable{
+    barberShop shop;
+    // Need access to shop object.
+    public Customer(barberShop shop){
+    this.shop = shop;
+    }
+    public void run(){
+    goForHairCut();
+    }
+    private void goForHairCut(){
+    shop.add(this);
+    }
+}
+    // CustomerGenerator that will become thread to start customer
+   // threads.
+class CustomerGenerator implements Runnable{
+    barberShop shop;
+    // Need access to shop object.
+    public CustomerGenerator(barberShop shop){
+    this.shop = shop;
+    }
+    public void run(){
+    while(true){
+    // Create customers and pass object “shop”
+    // Create thread
+    // start threads
+    // sleep random amount of time
+    }
+    } 
+}
+
+class barberShop{
+     int chairs = 0;
+     Semaphore customers;
+     Semaphore mutex;
+     public barberShop(int chairs)
+     {
+         this.chairs = chairs;
+         customers = new Semaphore(chairs);
+         mutex = new Semaphore(1);
+
+     }
+    // Initialize variables
+    // Initialize semaphores and mutex and waiting
+    public void cutHair(){
+    // Wait on customer
+    // Do things here like update number of customers waiting,
+    //signal to wake up barber, etc.
+    // Simulate cutting hair with sleep
+    }
+    public void add(Customer customer){
+    // Wait on entering the critical section
+    // DO THINGS HERE like determine if there are enough chairs
+   // in the waiting room. Leave if waiting room is full. If waiting room is
+    //not full, things must happen.
+    }
+}
