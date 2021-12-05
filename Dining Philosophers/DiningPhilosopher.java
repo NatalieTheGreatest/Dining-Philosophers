7e44d0ea7dc705bc0ac0e40b0a7e3bae8ad27162
nat <natalie.friede@gmail.com>
2021-12-05 16:19:23 -0600

    Olive Garden Implementation

--- Dining Philosophers/DiningPhilosopher.java
+++ Dining Philosophers/DiningPhilosopher.java
@@ -6,14 +6,46 @@ import java.util.Random;
 
 public class DiningPhilosopher {
     public static void main (String[] args){
-        //Table stores philosophers
-        Philosopher[] table = new Philosopher[5];
-        Chopstick[] chopsticks = new Chopstick[5];
-        for(int i = 0; i < 5; i++){
+        int runtime = 20;
+        try{
+            runtime = Integer.parseInt(args[0]);
+            }
+            catch(Exception e){
+                //Mostly for if they give random numbers or not enough args. Not a big deal
+                System.out.println("Integer args for runtime please");
+            }
+            //This is where a philosopher would go
+            Restaurant oliveGarden = new Restaurant(5, runtime);
+            Thread t = new Thread(oliveGarden);
+            t.start();
+        
+    }
+
+}
+
+//Somewhat extraneous, but it's fun to keep with the theme of dining
+class Restaurant implements Runnable 
+{
+private int philosophers, hours;
+
+public Restaurant(int philosphers, int hours)
+{
+    this.philosophers = philosphers;
+    this.hours = hours;
+}
+public Restaurant(){
+    philosophers = 5;
+    hours = 20;
+}
+    public void run()
+    {
+        Philosopher[] table = new Philosopher[this.philosophers];
+        Chopstick[] chopsticks = new Chopstick[this.philosophers];
+        for(int i = 0; i < this.philosophers; i++){
            chopsticks[i] = new Chopstick();
         }
         //Initializes the 4 normal philosophers
-        for(int i = 0; i < 4; i++){
+        for(int i = 0; i < this.philosophers - 1; i++){
             table[i] = new Philosopher(chopsticks[i], chopsticks[i+1]);
             Thread t = new Thread(table[i], "Philiosopher " + (i+1));
             t.start();
@@ -22,11 +54,20 @@ public class DiningPhilosopher {
         //They don't know left/right and have the chopsticks switched in their mind
         //My friend Kira does this all the time
         //This eliminates deadlock since you cannot have circular waiting this way
-        table[4] = new Philosopher(chopsticks[1], chopsticks[4]);
-        Thread t = new Thread(table[4], "Philiosopher " + 5);
+        table[4] = new Philosopher(chopsticks[1], chopsticks[philosophers - 1]);
+        Thread t = new Thread(table[philosophers - 1], "Philiosopher " + 5);
         t.start();
+        try{
+        Thread.sleep(hours*1000);
+        System.out.println("Restraunt is closed. Philosophers are ejected");
+        System.exit(0);
+        }
+        catch (Exception e){
+            
+        }
         
     }
+
 }
 
 class Philosopher implements Runnable {
